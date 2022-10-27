259b2990cf8c55fa503358fc079ae731a2a8c8f0
Yoselin Silva Jimenez <ysilvaj@MacBook-Pro-de-RI710472.local>
2022-10-27 12:28:54 -0300

    add: funcionalidades para trabjar mas facl y ademas un nuevo schema

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,16 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+export class Pokemon extends Document {
+    @Prop({
+        inique: true,
+        index: true,
+    })
+    name: string;
+    @Prop({
+        inique: true,
+        index: true,
+     })
+    no: number;
+}
+export const PokemonSchema =  SchemaFactory.createForClass(Pokemon);
