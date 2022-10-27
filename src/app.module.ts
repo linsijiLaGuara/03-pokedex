f5683f67ecb195f5f3d01f7b39af4050bbdd2ba6
Yoselin Silva Jimenez <ysilvaj@MacBook-Pro-de-RI710472.local>
2022-10-27 12:06:44 -0300

    add: dependencia configuradas de mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 
 @Module({
@@ -9,6 +11,8 @@ import { PokemonModule } from './pokemon/pokemon.module';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public')
      }),
+
+    MongooseModule.forRoot('mongodb://localhots:27017/nest-pokemon'),
     PokemonModule
     ],
 })
